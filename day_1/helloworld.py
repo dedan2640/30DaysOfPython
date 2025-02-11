5d45d5a5a30fd7773857229c12e02da04d4fadd1
dedan2640 <dedanireri2640@gmail.com>
2025-02-11 03:42:07 +0000

    Day one of 30DaysPython

---
+++ day_1/helloworld.py
@@ -0,0 +1,67 @@
+import math
+
+
+
+x = 3
+y = 4
+
+add = x+y
+print(f'Addition: {add}')
+
+subtract = x-y
+print(f'Subtraction: {subtract}')
+
+mult = x*y
+print(f'Multiplication: {mult}')
+
+modulus = y%x
+print(f'modulus: {modulus}')
+
+div = y/x
+print(f'Division: {div}')
+
+expo = y**x
+print(f'Exponential: {expo}')
+
+floordiv = y//x
+print(f'Floor division: {floordiv}')
+
+
+myname = "Dedan Ireri"
+print(myname)
+
+family_name = "Ngaruiya's family"
+print(family_name)
+
+mycountry = "kenya"
+print(mycountry)
+
+
+text = "I am enjoying 30 days of python"
+print(text)
+
+set = {"tote bag", "duffel", "backpack", "suitcase"}
+string ="i am a string"
+tuple = ("mazda", "toyota", "audi", "lexus")
+list = ["apple", "mango", "strawberry", "papai"]
+dictionary = {"item": "bag", "color":"black", "price":"Ksh 700"}
+integer = 4
+float = 5.67
+complex = complex(3,5) #a complex number with real and imaginary part
+boolean = True
+
+
+#Euclidian distance between two points
+point1 = [2,3]
+point2 = [10,8]
+
+#print= (math.dist(point1 , point2))
+
+
+print(type(10))
+print(type(9.8))
+print(type(3.14))
+print(type(34-4j))
+b = print(type(myname))
+print(type(family_name))
+print(type(mycountry))
