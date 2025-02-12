c966854f8137e92fa9bd4d4ece7b7fa18fd87b0c
dedan2640 <dedanireri2640@gmail.com>
2025-02-12 22:08:53 +0300

    Day two assignment

---
+++ day_2/variables.py
@@ -0,0 +1,13 @@
+#Day 2: 30 Days of python programming
+first_name = "dedan"
+last_name = "ireri"
+full_name = "dedan ireri ngaruiya"
+country = "kenya"
+city = "nairobi"
+age = 18
+year = "2024"
+is_married = False
+is_true = True
+is_light_on = "yes"
+
+item_name, colour, price = "Samsung A15", "blue", "Ksh 26,400"
