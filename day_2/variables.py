5518f1b2fa911c095e2001392fe93dd525c176fa
dedan2640 <dedanireri2640@gmail.com>
2025-02-12 22:38:21 +0300

    Day two python assignment

--- day_2/variables.py
+++ day_2/variables.py
@@ -11,3 +11,42 @@ is_true = True
 is_light_on = "yes"
 
 item_name, colour, price = "Samsung A15", "blue", "Ksh 26,400"
+type(first_name)
+type(last_name)
+type(full_name)
+type(country) 
+type(city) 
+type(age)
+type(year)
+type(is_married) 
+type(is_true)
+type(is_light_on)
+type(item_name) 
+type(colour) 
+type(price)
+
+len(first_name)
+
+num_one = 5
+num_two = 4
+total = num_one + num_two
+diff = num_one - num_two
+product = num_two * num_one
+division = num_one/num_two
+remainder = num_two%num_one
+floor_division = num_one//num_two
+
+#cirle
+radius = 30 #in meters
+area_of_circle = 22/7 * radius* radius
+circum_of_circle = 22/7 * 2(radius)
+
+user_radius = int(input("Enter radius of circle: "))
+areaofCirle =22/7 * user_radius * user_radius
+
+
+first_name = input("Whats your first name?")
+second_name = input("Whats your second name?")
+country = input("Enter your country name: ")
+age = input("Enter your age: ")
+
