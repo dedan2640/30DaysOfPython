7693091ef1759a9afeed86b8cbe9d78ca2dac508
dedan2640 <dedanireri2640@gmail.com>
2025-02-13 10:18:24 +0300

    JavaScript Day Two assignment

---
+++ day_2/trial.js
@@ -0,0 +1,41 @@
+var challenge = "30 Days of JavaScript";
+console.log(challenge)
+
+console.log(challenge.length)
+
+challenge.toUpperCase
+challenge.toLowerCase
+challenge.substr(0,4)
+challenge.slice(3)
+challenge.includes("Script")
+challenge.split(" ")
+
+
+let companies = "'Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon";
+companies.split(" ")
+challenge.replace("JavaScript","Python")
+challenge.charAt(15)
+challenge.charCodeAt()
+challenge.indexOf("a")
+challenge.lastindexof("a")
+
+
+let sentence ="You cannot end a sentence with because because because is a conjunction";
+sentence.lastIndexOf("because")
+sentence.search("because")
+
+let text = "30 Days Of JavaScript"
+text.startsWith("30",0)
+text.endsWith("JavaScript",1)
+text.match("a")
+
+let text1 = "30 Days of"
+let text2 = "JavaScript"
+text1.concat("",text2)
+
+console.log(text.repeat(2))
+
+
+
+console.log("The quote 'There is no exercise better for the heart than reaching down and lifting people up.' by John Holmes teaches us to help one another.")
+console.log("Love is not patronizing and charity isn't about pity, it is about love. Charity and love are the same -- with charity you give love, so don't just give money but reach out your hand instead.")
