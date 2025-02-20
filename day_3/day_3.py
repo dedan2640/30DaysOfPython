b86585f1119897690c8f4596a99b01b9cc50885e
dedan2640 <dedanireri2640@gmail.com>
2025-02-20 13:06:01 +0000

    day 3

---
+++ day_3/day_3.py
@@ -0,0 +1,32 @@
+age = 18
+height = 5.6
+complex = (5,6)
+
+#triangle
+base = int(input("Enter base of the triangle: "))
+height = int(("Enter the height of the triangle: "))
+area = 0.5 * base * height
+
+#perimeter of triangle
+a = int(input("Enter side of triangle: "))
+b = int(input("Enter second side of triangle:"))
+c = int(input("Enter third side of triangle: "))
+
+perimeter = a + b + c
+
+
+#rectangle
+length= int(input("Enter length of the rectangle: "))
+width = int(input("Enter width of the triangle: "))
+
+#cicle
+PI = 3.142
+radius = int(input("Enter the radius: "))
+circumference = 2 *PI * radius
+area = PI * radius * radius
+
+point1 = x=3 ,y=3
+
+
+
+
